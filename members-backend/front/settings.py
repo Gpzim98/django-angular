efae3add457bef44b87988a5a917b04b6d10a45a
Gregory Pacheco <gpzim98@gmail.com>
2019-11-12 00:12:02 +0000

    Profile picture

--- members-backend/front/settings.py
+++ members-backend/front/settings.py
@@ -127,3 +127,9 @@ STATIC_URL = '/static/'
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:4200",
 ]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- members-backend/front/urls.py
+++ members-backend/front/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from core.views import MemberViewSet
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'members', MemberViewSet)
@@ -11,4 +12,4 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
