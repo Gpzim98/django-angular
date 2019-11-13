28ff5f44454e031e40f49b43fb502a068444a821
Gregory Pacheco <gpzim98@gmail.com>
2019-11-13 16:10:36 +0000

    Utilizando o postgres

--- members-backend/front/settings.py
+++ members-backend/front/settings.py
@@ -86,6 +86,14 @@ WSGI_APPLICATION = 'front.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'django',
+        'PASSWORD': 'djangosenha',
+        'HOST': 'localhost',
+        'PORT': '',
+    },
+    'local': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
